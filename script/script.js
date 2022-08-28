f925ec55ccf344d64c6d70fdfd969e4b18a31090
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-08-28 21:33:36 +0530

    Basic setup in scipt.js

---
+++ script/script.js
@@ -0,0 +1,38 @@
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = 
+[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [6,4,2]
+]
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame()
+{
+    document.querySelector(".endgame").style.display="none";
+    origBoard = Array.from(Array(9).keys());
+    for(var i=0;i<cells.length;i++)
+    {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click',nextTurn,false);
+    }
+}
+
+function nextTurn(square)
+{
+    turn(square.target.id,huPlayer);
+}
+
+function turn(squareID,player)
+{
+    origBoard[squareID] = player;
+    document.getElementById(squareID).innerText=player;
+}
